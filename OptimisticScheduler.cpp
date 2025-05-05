cdbf6dd301473d0265783fd463926a643a6572c6
the-vvong-path <gayathrishreeyapatnala@gmail.com>
2025-05-06 02:08:23 +0530

    Fix Optimistic Scheduler logic

--- OptimisticScheduler.cpp
+++ OptimisticScheduler.cpp
@@ -1,7 +1,11 @@
 #include "OptimisticScheduler.h"
 #include <iostream>
+#include "Logger.h"
+
+static Logger LOGGER;
 
 OptimisticScheduler::OptimisticScheduler() : counter(1) {
+    LOGGER.filenum = 2;
     G = new WaitsForGraph();
 }
 
@@ -25,12 +29,15 @@ Transaction* OptimisticScheduler::begin_trans(int threadID) {
 
 bool OptimisticScheduler::read(Transaction* t, int index, int &localVal) {
 
+    LOGGER.OUTPUTT("    t", t->transactionId, " is trying to read from [", index, "]");
     std::unique_lock<std::mutex> lock(shared[index]->datalock);
     localVal = shared[index]->value;
     shared[index]->readList.insert(t->transactionId);
+    LOGGER.OUTPUTT("    t", t->transactionId, " inserted in the readList of [", index, "]");
     for (auto it: shared[index]->writeList) {
         if (t->transactionId != it) {
             t->dependencySet.insert(it);
+            LOGGER.OUTPUTT("    t", t->transactionId, " inserted ", it, " in the dependencySet");
         }
     }
     return true;
@@ -40,23 +47,27 @@ bool OptimisticScheduler::write(Transaction* t, int index, int localVal) {
     // local writes
     t->localWrites[index] = localVal;
     std::unique_lock<std::mutex> lock(shared[index]->datalock);
+    shared[index]->writeList.insert(t->transactionId);
+    LOGGER.OUTPUTT("    t", t->transactionId, " inserted in the writeList of [", index, "]");
     for (auto it: shared[index]->readList) {
         if (t->transactionId != it) {
             t->dependencySet.insert(it);
+            LOGGER.OUTPUTT("    t", t->transactionId, " inserted ", it, " in the dependencySet");
         }
     }
     for (auto it: shared[index]->writeList) {
         if (t->transactionId != it) {
             t->dependencySet.insert(it);
+            LOGGER.OUTPUTT("    t", t->transactionId, " inserted ", it, " in the dependencySet");
         }
     }
-    shared[index]->writeList.insert(t->transactionId);
     return true;
 }
 
 TransactionStatus OptimisticScheduler::tryCommit(Transaction* t) {
+    LOGGER.OUTPUTT("    t", t->transactionId, " is trying to commit");
     // Check if the transaction causes a cycle in the waits-for graph
-    std::unique_lock<std::mutex> lock(graphLock);
+    graphLock.lock();
     for (auto [index, value]: t->localWrites) {
         bool permission = G->addWriteOperation(t->transactionId, shared[index].get());
         if (!permission) {
@@ -65,11 +76,14 @@ TransactionStatus OptimisticScheduler::tryCommit(Transaction* t) {
             deleteAllWrites(t);
             std::lock_guard<std::mutex> lock1(commitLock);
             committedTransactions.insert(t->transactionId);
+            LOGGER.OUTPUTT("    t", t->transactionId, " aborted due to cycle in the WFG, added to committedTransactions");
+            graphLock.unlock();
             return aborted;
         }
     }
+    graphLock.unlock();
 
-    std::cout << t->transactionId << std::endl;
+    LOGGER.OUTPUTT("    t", t->transactionId, " got permission to commit from WFG, starting the wait for dependencies");;
 
     // Wait until all the dependencies are committed
     while(true) {
@@ -80,14 +94,16 @@ TransactionStatus OptimisticScheduler::tryCommit(Transaction* t) {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
-    std::cout << t->transactionId << std::endl;
+    LOGGER.OUTPUTT("    t", t->transactionId, " has no remaining dependencies, proceeding to commit");
 
     for (auto [index, value]: t->localWrites) {
         std::unique_lock<std::mutex> lock(shared[index]->datalock);
         shared[index]->value = value;
+        LOGGER.OUTPUTT("    t", t->transactionId, " wrote to [", index, "] a value ", value);
     }
     std::lock_guard<std::mutex> lock1(commitLock);
     committedTransactions.insert(t->transactionId);
+    LOGGER.OUTPUTT("    t", t->transactionId, " committed successfully, added to committed transactions");
     return committed;
 }
 
