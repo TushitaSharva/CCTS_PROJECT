dd6b6275bbd265a8de272e5c3a9d3c196e0a63e3
TushitaSharva <tushitasharva03@gmail.com>
2025-05-06 13:47:11 +0530

    Final script file

--- run.sh
+++ run.sh
@@ -7,21 +7,33 @@ if [ -d "$outputs_directory" ]; then
 fi
 
 # Input file
-# input_file_name="inp.txt"
-# g++ -std=c++17 O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp Scheduler.cpp -o a.out
-# g++ -std=c++17 O-O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp OptimisticScheduler.cpp -o a.out
-# valgrind ./a.out "inp.txt"
+input_file_name="inp.txt"
+g++ -std=c++17 O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp Scheduler.cpp -o o2pl.out
+g++ -std=c++17 O-O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp OptimisticScheduler.cpp -o oo2pl.out
+./o2pl.out "inp.txt"
+./oo2pl.out "inp.txt"
 
-# g++ -std=c++17 O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp Scheduler.cpp -o a.out
-g++ -std=c++17 O-O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp OptimisticScheduler.cpp -o a.out
+# g++ -std=c++17 O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp Scheduler.cpp -o o2pl.out
+# g++ -std=c++17 O-O2PL.cpp NodeList.cpp DataItem.cpp WaitsForGraph.cpp OptimisticScheduler.cpp -o oo2pl.out
 # Uncomment lines 37-47 to run loops
-for input_file_name in ./inputs/*/*.txt; do
-    echo "Experiment: $input_file_name"
-    ./a.out "$input_file_name"
-    echo ""
+# for input_file_name in ./inputs/E2/*.txt; do
+#     echo "Experiment: $input_file_name"
+#     ./a.out "$input_file_name"
+#     echo ""
 
-    outputs_directory="./outputs"
-    if [ -d "$outputs_directory" ]; then
-        rm -rf "$outputs_directory"/*
-    fi
-done
+#     outputs_directory="./outputs"
+#     if [ -d "$outputs_directory" ]; then
+#         rm -rf "$outputs_directory"/*
+#     fi
+# done
+
+# for input_file_name in ./inputs/E3/*.txt; do
+#     echo "Experiment: $input_file_name"
+#     ./a.out "$input_file_name"
+#     echo ""
+
+#     outputs_directory="./outputs"
+#     if [ -d "$outputs_directory" ]; then
+#         rm -rf "$outputs_directory"/*
+#     fi
+# done
